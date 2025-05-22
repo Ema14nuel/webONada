be2057ef4111695f538723fd45a7096e50538ee8
Ema14nuel <fdsolinf@gmail.com>
2025-05-21 20:48:43 -0500

    Generación de productos listado y filtro.

--- webONada/apps/products/views/views.py
+++ webONada/apps/products/views/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from apps.products.forms.ProductForms import ProductFilter
+
+#models
+from apps.products.models import Product, Category, ProductPresentation
 
 # Create your views here.
 #site products
@@ -6,7 +10,18 @@ def products(request):
     """
     Render the services page.
     """
-    return render(request, 'products/products.html')
+    # form de productos
+    form = ProductFilter()
+
+    # listado de productos
+    products = ProductPresentation.objects.all()
+
+    context = {
+        'form': form,
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
 
 # site products details
 def products_datail(request):
