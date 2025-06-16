77b00632e10fa931ada7d4d5252730c175d63966
Fernando Díaz <fdsolinf@gmail.com>
2025-06-15 21:59:10 -0500

    Se realiza pagina de categorias y detalle de la misma.

--- webONada/apps/products/urls.py
+++ webONada/apps/products/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     # path( url_principal+'crear', views.candidato_crear, name='candidato_crear'),
     path(url_principal+'', views.products, name='products'),
     path(url_principal+'detalle/<int:pk>', views.products_detail, name='products_detail'),
+    path(url_principal+'categorias/', views.categories, name='categories'),
+    path(url_principal+'categorias/detalle/<int:pk>', views.categories_detail, name='categories_detail'),
 
 ]
--- webONada/apps/products/views/views.py
+++ webONada/apps/products/views/views.py
@@ -37,3 +37,31 @@ def products_detail(request, pk):
     }
 
     return render(request, 'products/products_detail.html', context)
+
+#site categories
+def categories(request):
+    """
+    Render the categories page.
+    """
+    # listado de categorias
+    categories = Category.objects.all()
+
+    context = {
+        'categories': categories,
+    }
+
+    return render(request, 'products/categories.html', context)
+
+#site categories details
+def categories_detail(request, pk):
+    """
+    Render the categories detail page.
+    """
+    # listado de categorias
+    category = Category.objects.get(id=pk)
+
+    context = {
+        'category': category,
+    }
+
+    return render(request, 'products/categories_detail.html', context)
